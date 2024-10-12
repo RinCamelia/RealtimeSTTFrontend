e8e382485241a3f02cc49991671fc3089c45329f
Rin Camelia <10214664+RinCamelia@users.noreply.github.com>
2024-10-12 13:54:21 -0500

    replace " with ' and move config loading into a separate function

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-if __name__ == '__main__':
-
-    import os
-    import sys
-    import keyboard
-    import yaml
-    from RealtimeSTT import AudioToTextRecorder 
-
-    if os.name == "nt" and (3, 8) <= sys.version_info < (3, 99):
-        from torchaudio._extension.utils import _init_dll_path
-        _init_dll_path()
-
-    config_file_name = 'config.yml'
+def load_config(config_file_name):
     config = {
         'model': 'small.en',
         'language': 'en',
@@ -27,6 +15,22 @@ if __name__ == '__main__':
     else:
         with open(config_file_name, 'w') as config_file:
             yaml.safe_dump(config, config_file)
+    return config
+
+if __name__ == '__main__':
+
+    import os
+    import sys
+    import keyboard
+    import yaml
+    from RealtimeSTT import AudioToTextRecorder 
+
+    if os.name == 'nt' and (3, 8) <= sys.version_info < (3, 99):
+        from torchaudio._extension.utils import _init_dll_path
+        _init_dll_path()
+
+    config_file_name = 'config.yml'
+    config = load_config(config_file_name)
 
     recorder = AudioToTextRecorder(
         spinner=config['enable_spinner'],
@@ -39,23 +43,23 @@ if __name__ == '__main__':
     lines_storage = []
 
     def process_text(text):
-        print("Transcribed line: " + text)
-        lines_storage.append(text + "\n")
+        print('Transcribed line: ' + text)
+        lines_storage.append(text + '\n')
 
     def dump_lines():
         if len(lines_storage) > 0:
-            print("writing lines...")
+            print('writing lines...')
             for line in lines_storage:
                 keyboard.write(line)
                 time.sleep(config['dump_time_between_lines'])
             lines_storage.clear()
 
 
-    print("start commenting...")
+    print('start commenting...')
     keyboard.add_hotkey(config['dump_hotkey'], dump_lines)
     
     try:
         while (True):
             recorder.text(process_text)
     except KeyboardInterrupt:
-        print("Exiting application due to keyboard interrupt")
+        print('Exiting application due to keyboard interrupt')
