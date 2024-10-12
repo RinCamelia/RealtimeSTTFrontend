620a93a043dc3b6fced6d2f5a3e8ed899388dc70
Rin Camelia <10214664+RinCamelia@users.noreply.github.com>
2024-10-12 17:21:41 -0500

    reorganize config file

--- main.py
+++ main.py
@@ -32,18 +32,24 @@ class DumpMethod(Enum):
 yaml.register_class(DumpMethod)
 
 def load_config(config_file_name):
-    config = dict(
-        version=3,
-        model='small.en',
-        language='en',
-        prompt='',
-        enable_spinner=False,
-        speech_model_sensitivity=0.05,
-        dump_hotkey=';',
-        dump_time_between_lines= 0.1,
-        dump_method=DumpMethod.KEYBOARD,
-        dump_file_name='transcribed_text.txt'
-    )
+    config = {
+        'version': 4,
+        'stt_settings': {
+            'model': 'small.en',
+            'language': 'en',
+            'prompt': None,
+            'enable_spinner': False,
+            'speech_model_sensitivity': 0.05,
+        },
+        'hotkeys': {
+            'text_output': ';'
+        },
+        'text_output': {
+            'time_between_lines': 0.1,
+            'method': DumpMethod.KEYBOARD,
+            'file_name': 'transcribed_text.txt'
+        }
+    }
 
     if (os.path.isfile(config_file_name)):
         with open(config_file_name) as config_file:
@@ -67,13 +73,13 @@ def process_text(text):
 def dump_lines(method):
     if len(lines_storage) > 0:
         print('writing lines via chosen dump method')
-        match config['dump_method']:
+        match config['text_output']['method']:
             case DumpMethod.KEYBOARD:
                 for line in lines_storage:
                     keyboard.write(line)
-                    time.sleep(config['dump_time_between_lines'])
+                    time.sleep(config['text_output']['time_between_lines'])
             case DumpMethod.TEXT_FILE:
-                with open(config['dump_file_name'], 'a') as transcription_file:
+                with open(config['text_output']['file_name'], 'a') as transcription_file:
                     transcription_file.writelines(lines_storage)
         lines_storage.clear()
 
@@ -87,15 +93,15 @@ if __name__ == '__main__':
         _init_dll_path()
 
     recorder = AudioToTextRecorder(
-        spinner=config['enable_spinner'],
-        silero_sensitivity=config['speech_model_sensitivity'],
-        model=config['model'],
-        language=config['language'],
-        initial_prompt=config['prompt']
+        spinner=config['stt_settings']['enable_spinner'],
+        silero_sensitivity=config['stt_settings']['speech_model_sensitivity'],
+        model=config['stt_settings']['model'],
+        language=config['stt_settings']['language'],
+        initial_prompt=config['stt_settings']['prompt']
         )
 
     print('start commenting...')
-    keyboard.add_hotkey(config['dump_hotkey'], dump_lines)
+    keyboard.add_hotkey(config['hotkeys']['text_output'], dump_lines)
     
     try:
         while (True):
